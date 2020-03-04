782cfef43e3c325c4caadde1ae6268905b716814
CUN1NGER <jayantschuil@hotmail.com>
2020-03-04 22:26:21 +0000

    added range input

--- sketch.js
+++ sketch.js
@@ -1,22 +1,25 @@
 function selectPhone(){
-    var body = document.getElementById("jeetjejoh");
+    var body = document.getElementsByClassName("dropDown");
     function createSelect(name, numCount){
         var select = document.createElement("select");
         select.id = name;
-        body.appendChild(select);
-    
+        body[0].appendChild(select);
+        
         for(var i = 0; i < numCount; i++){
             var option = document.createElement("option");
             option.value = i;
             option.text = i;
             select.appendChild(option);
         }
-    
+        
     }
     
-            
+    
     for(var i = 0; i < 3; i++){
         createSelect(i, 9999);
     }
 }
 
+function updateTextInput(val) {
+    document.getElementById('textInput').value=val; 
+}
