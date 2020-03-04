dca0c39068ba9fe86e0494aff846e493ca24e444
Jayant Schuil <jayantschuil@hotmail.com>
2020-03-04 16:37:07 +0100

    added all necessary files
    jeetje joh

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var body = document.getElementById("jeetjejoh");
+function createSelect(name, numCount){
+    var select = document.createElement("select");
+    select.id = name;
+    body.appendChild(select);
+
+    for(var i = 0; i < numCount; i++){
+        var option = document.createElement("option");
+        option.value = i;
+        option.text = i;
+        select.appendChild(option);
+    }
+
+}
+
+        
+for(var i = 0; i < 3; i++){
+    createSelect(i, 9999);
+}
