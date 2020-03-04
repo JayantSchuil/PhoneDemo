062b6ccd0f8689f04142835dbd27305b404ae101
CUN1NGER <jayantschuil@hotmail.com>
2020-03-04 21:58:22 +0000

    made it easier to add more options

--- sketch.js
+++ sketch.js
@@ -1,19 +1,22 @@
-var body = document.getElementById("jeetjejoh");
-function createSelect(name, numCount){
-    var select = document.createElement("select");
-    select.id = name;
-    body.appendChild(select);
-
-    for(var i = 0; i < numCount; i++){
-        var option = document.createElement("option");
-        option.value = i;
-        option.text = i;
-        select.appendChild(option);
+function selectPhone(){
+    var body = document.getElementById("jeetjejoh");
+    function createSelect(name, numCount){
+        var select = document.createElement("select");
+        select.id = name;
+        body.appendChild(select);
+    
+        for(var i = 0; i < numCount; i++){
+            var option = document.createElement("option");
+            option.value = i;
+            option.text = i;
+            select.appendChild(option);
+        }
+    
     }
-
+    
+            
+    for(var i = 0; i < 3; i++){
+        createSelect(i, 9999);
+    }
 }
 
-        
-for(var i = 0; i < 3; i++){
-    createSelect(i, 9999);
-}
